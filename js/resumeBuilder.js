19ff47fb3baccea770d8681e4489d451bcd8b57c
gusboh <gusboh@gmail.com>
2017-01-11 16:18:51 +0100

    Udacity - String Manipulation 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,39 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+//$("#main").append("Gustavo Bohlen");
+
+var name = "Gustavo Bohlen";
+
+var role = "Imagineere" 
+
+//var age = 29;
+
+//var awesomeThoughts = "I am Gustavo and I am AWESOME!";
+
+//var funThoughts = awesomeThoughts.replace ("AWESOME", "FUN");
+
+//formattedName = HTMLheaderName.replace("%data%", name);
+
+//formattedRole = HTMLheaderRole.replace("%data%", role)
+
+//console.log (awesomeThoughts);
+
+//console.log (funThoughts);
+
+//$("#main").append(funThoughts);
+
+//$(« #header").prepend(formattedRole);
+
+//$(« #header").prepend(formattedName);
+
+var s = "audacity";
+
+var udacityizer = function(s) {  
+    // Right now, the variable s === "audacity"
+    // Manipulate s to make it equal to "Udacity"
+    // Your code goes here!
+    var s.replace("audacity", "Udacity");
+    
+    return s;
+};
+
+// Did your code work? The line below will tell you!
+console.log(udacityizer(s));
